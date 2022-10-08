3190ccfda3887fafd9c2fc9632005c000234df21
Jeffrey Kimani Wainaina <112941886+jeffkimani@users.noreply.github.com>
2022-10-08 22:25:14 +0300

    Add files via upload

---
+++ love_calculator.py
@@ -0,0 +1,7 @@
+# A program that calculates love percentage by using names
+#name input
+first_person_name = input("Enter her name: \n")
+second_person_name = input("Enter his name: \n")
+#love calculation
+first_person_name.count("T","R","U","E","L","O","V","E")
+second_person_name.count("T","R","U","E","L","O","V","E")
